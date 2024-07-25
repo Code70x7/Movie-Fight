3dcd0b8ef0c5ea87b8a5885fdc595be52ad2017b
Tony Dayton <tonylynndayton@yahoo.com>
2024-07-25 11:07:59 -0600

    added comments

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fetchData = async () => {
+    //await is to keep the code going (asynchronous)
     const response = await axios.get('http://www.omdbapi.com/', {
+        //params attaches query string(appends) these on the end of the url
         params: {
             apikey: '332a1af0',
             s: 'avengers'
         }
     });
+    //data is the parameter to the response object
     console.log(response.data);
 };
+//calling the function
 fetchData();
